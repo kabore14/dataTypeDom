cc2b7c29b1aca9757cb47184814947e82bbb9e26
“kabore14” <wend.kisito@gmail.com>
2018-10-15 02:17:59 -0400

    first commit

---
+++ script.js
@@ -0,0 +1,15 @@
+
+
+// get name from prompt and saves it 
+var user = prompt( "Enter Name");
+
+// gets h1 element with id 'message'
+let messageH1 = document.getElementById('message');
+//edits text of h1 element
+messageH1.innerHTML = 'welcome ' + user;
+
+
+
+
+ 
+
